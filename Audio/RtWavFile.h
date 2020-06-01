aeb4c884928e6a142dd6c29d111a6198fcf79419
jt396 <j.taylor.m91@gmail.com>
2020-06-01 22:22:44 +0100

    Test format fix; this code is old but the formatting annoys me.

--- Audio/RtWavFile.h
+++ Audio/RtWavFile.h
@@ -1,12 +1,12 @@
 /*
 ===============================================================================
 
-	ReflecTech
-	==========
-	File		:	RtWavFile.h
-	Author		:	Jamie Taylor
-	Last Edit	:	25/03/13
-	Desc		:	A basic static wav file class. Adopting a DOOM 3 style syntax.
+    ReflecTech
+    ==========
+    File        :   RtWavFile.h
+    Author      :   Jamie Taylor
+    Last Edit   :   25/03/13
+    Desc        :   A basic static wav file class. Adopting a DOOM 3 style syntax.
 
 ===============================================================================
 */
@@ -31,30 +31,30 @@ Wave file class
 */
 class WavFile {
 public:
-					//WaveFile( void );
-	virtual			~WavFile( void ) { };
+                    //WaveFile( void );
+    virtual         ~WavFile( void ) { };
 
-	virtual bool	Load( const I8 *fileName ) = 0;
-	virtual void	UnLoad( void ) = 0;
+    virtual bool    Load( const I8 *fileName ) = 0;
+    virtual void    UnLoad( void ) = 0;
 
-	virtual void	Play( void ) = 0;
-	virtual void	Pause( void ) = 0;
-	virtual void	Stop( void ) = 0;
+    virtual void    Play( void ) = 0;
+    virtual void    Pause( void ) = 0;
+    virtual void    Stop( void ) = 0;
 
-					// loopStart and loopDuration expect a number of seconds
-	virtual void	SetLoopingParams( U32 loopStart, U32 loopDuration, U32 loopCount, bool immediateEffect = true ) = 0;
-					
-					// within the range 0.0f - 1.0f
-	virtual F32		GetVolume( void ) const = 0;
-	virtual void	SetVolume( F32 vol ) = 0;
+                    // loopStart and loopDuration expect a number of seconds
+    virtual void    SetLoopingParams( U32 loopStart, U32 loopDuration, U32 loopCount, bool immediateEffect = true ) = 0;
+                    
+                    // within the range 0.0f - 1.0f
+    virtual F32     GetVolume( void ) const = 0;
+    virtual void    SetVolume( F32 vol ) = 0;
 
-	virtual bool	IsLoaded( void ) const = 0;
-	virtual bool	IsPlaying( void ) const = 0;
+    virtual bool    IsLoaded( void ) const = 0;
+    virtual bool    IsPlaying( void ) const = 0;
 
 protected:
-	bool			isLoaded;
-	bool			isPlaying;
-	U8 *			audioBuffer;
+    bool            isLoaded;
+    bool            isPlaying;
+    U8*             audioBuffer;
 };
 
 
